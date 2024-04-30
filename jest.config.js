f915badd2de4e2141cfb733e1591a1ca924b46e3
Magnun A V F <magnun.furtado@azion.com>
2024-04-30 14:35:17 -0300

    chore: set jest test configs

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    './photon/lib/index': '<rootDir>/__mocks__/photonLib.ts',
+  },
+  transform: {
+    '^.+\\.(t|j)s?$': '@swc/jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   testEnvironment: 'node',
 };
