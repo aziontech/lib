0fba4229250fe843f8e0156ec6f5933cd66aee9c
Magnun A V F <magnun.furtado@azion.com>
2024-10-10 17:16:19 -0300

    test: create tests for custom fetch

--- packages/domains/src/utils/index.ts
+++ packages/domains/src/utils/index.ts
@@ -34,32 +34,26 @@ export const limitArraySize = <T>(array: T[], limit: number): T[] => {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function fetchWithErrorHandling(url: string, options?: RequestInit, debug?: boolean): Promise<any> {
-  try {
-    const response = await fetch(url, options);
+  const response = await fetch(url, options);
 
-    if (!response.ok) {
-      const msg = `HTTP error! Status: ${response.status} - ${response.statusText}`;
+  if (!response.ok) {
+    const msg = `HTTP error! Status: ${response.status} - ${response.statusText}`;
 
-      if (debug) console.log(`Error in fetch: ${msg}`);
+    if (debug) console.log(`Error in fetch: ${msg}`);
 
-      throw new Error(msg);
-    }
-
-    const contentType = response.headers.get('content-type');
-    if (!contentType || !contentType.includes('application/json')) {
-      const textResponse = await response.text();
-      const msg = `Expected JSON response, but got: ${textResponse}`;
-
-      if (debug) console.log(`Error in fetch: ${msg}`);
-
-      throw new Error(msg);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    if (debug) console.log(`Error in fetch: ${err}`);
-
-    throw err;
+    throw new Error(msg);
   }
+
+  const contentType = response.headers.get('content-type');
+  if (!contentType || !contentType.includes('application/json')) {
+    const textResponse = await response.text();
+    const msg = `Expected JSON response, but got: ${textResponse}`;
+
+    if (debug) console.log(`Error in fetch: ${msg}`);
+
+    throw new Error(msg);
+  }
+
+  const data = await response.json();
+  return data;
 }
