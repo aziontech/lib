a2a743eba1363538e4604446de8f8f0c4fabed6d
José Filho <jc.bsfilho@gmail.com>
2025-01-15 11:46:13 -0300

    feat: add firewall strategy to process config context

--- packages/config/src/configProcessor/processStrategy/index.ts
+++ packages/config/src/configProcessor/processStrategy/index.ts
@@ -7,6 +7,7 @@ import RulesProcessConfigStrategy from '../processStrategy/implementations/rules
 import NetworkListProcessConfigStrategy from '../processStrategy/implementations/secure/networkListProcessConfigStrategy';
 import WafProcessConfigStrategy from '../processStrategy/implementations/secure/wafProcessConfigStrategy';
 import ProcessConfigContext from '../processStrategy/processConfigContext';
+import FirewallProcessConfigStrategy from './implementations/secure/firewallProcessConfigStrategy';
 
 function factoryProcessContext() {
   const processConfigContext = new ProcessConfigContext();
@@ -17,6 +18,7 @@ function factoryProcessContext() {
   processConfigContext.setStrategy('purge', new PurgeProcessConfigStrategy());
   processConfigContext.setStrategy('networkList', new NetworkListProcessConfigStrategy());
   processConfigContext.setStrategy('waf', new WafProcessConfigStrategy());
+  processConfigContext.setStrategy('firewall', new FirewallProcessConfigStrategy());
   // Rules must be last to apply to behaviors (origin, cache...)
   processConfigContext.setStrategy('rules', new RulesProcessConfigStrategy());
   return processConfigContext;
