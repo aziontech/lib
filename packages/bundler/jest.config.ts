48be0dd9b97bf853e7163fd38e04b2a79f7439ec
José Filho <jc.bsfilho@gmail.com>
2025-02-11 12:46:51 -0300

    chore: update TypeScript configuration and add Jest support for bundler

---
+++ packages/bundler/jest.config.ts
@@ -0,0 +1,25 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: JestConfigWithTsJest = {
+  displayName: 'Bundler',
+  clearMocks: true,
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ['node_modules/'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+};
+
+export default config;
