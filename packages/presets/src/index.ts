1025ad3d9025fea561399b59e4e2a40896456b23
João Narciso <joaoznarciso@gmail.com>
2025-03-05 17:28:58 -0300

    refactor(presets): convert preset names to lowercase

--- packages/presets/src/index.ts
+++ packages/presets/src/index.ts
@@ -1,71 +1,71 @@
-import Angular from './presets/angular';
-import Astro from './presets/astro';
-import Docusaurus from './presets/docusaurus';
-import Eleventy from './presets/eleventy';
-import Emscripten from './presets/emscripten';
-import Gatsby from './presets/gatsby';
-import Hexo from './presets/hexo';
-import Html from './presets/html';
-import Hugo from './presets/hugo';
-import JavaScript from './presets/javascript';
-import Jekyll from './presets/jekyll';
-import Next from './presets/next';
-import Nuxt from './presets/nuxt';
-import Preact from './presets/preact';
-import Qwik from './presets/qwik';
-import React from './presets/react';
-import RustWasm from './presets/rustwasm';
-import Svelte from './presets/svelte';
-import TypeScript from './presets/typescript';
-import VitePress from './presets/vitepress';
-import Vue from './presets/vue';
+import angular from './presets/angular';
+import astro from './presets/astro';
+import docusaurus from './presets/docusaurus';
+import eleventy from './presets/eleventy';
+import emscripten from './presets/emscripten';
+import gatsby from './presets/gatsby';
+import hexo from './presets/hexo';
+import html from './presets/html';
+import hugo from './presets/hugo';
+import javascript from './presets/javascript';
+import jekyll from './presets/jekyll';
+import next from './presets/next';
+import nuxt from './presets/nuxt';
+import preact from './presets/preact';
+import qwik from './presets/qwik';
+import react from './presets/react';
+import rustwasm from './presets/rustwasm';
+import svelte from './presets/svelte';
+import typescript from './presets/typescript';
+import vitepress from './presets/vitepress';
+import vue from './presets/vue';
 
 const presets = {
-  Angular,
-  Astro,
-  Docusaurus,
-  Eleventy,
-  Emscripten,
-  Gatsby,
-  Hexo,
-  Html,
-  Hugo,
-  JavaScript,
-  Jekyll,
-  Next,
-  Nuxt,
-  Preact,
-  Qwik,
-  React,
-  RustWasm,
-  Svelte,
-  TypeScript,
-  VitePress,
-  Vue,
-};
+  angular,
+  astro,
+  docusaurus,
+  eleventy,
+  emscripten,
+  gatsby,
+  hexo,
+  html,
+  hugo,
+  javascript,
+  jekyll,
+  next,
+  nuxt,
+  preact,
+  qwik,
+  react,
+  rustwasm,
+  svelte,
+  typescript,
+  vitepress,
+  vue,
+} as const;
 
 export {
-  Angular,
-  Astro,
-  Docusaurus,
-  Eleventy,
-  Emscripten,
-  Gatsby,
-  Hexo,
-  Html,
-  Hugo,
-  JavaScript,
-  Jekyll,
-  Next,
-  Nuxt,
-  Preact,
-  Qwik,
-  React,
-  RustWasm,
-  Svelte,
-  TypeScript,
-  VitePress,
-  Vue,
+  angular,
+  astro,
+  docusaurus,
+  eleventy,
+  emscripten,
+  gatsby,
+  hexo,
+  html,
+  hugo,
+  javascript,
+  jekyll,
+  next,
+  nuxt,
+  preact,
+  qwik,
+  react,
+  rustwasm,
+  svelte,
+  typescript,
+  vitepress,
+  vue,
 };
 
 export default presets;
