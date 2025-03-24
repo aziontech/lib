937195061a99bfe2efd59b5fb4bd858fb27dfb8f
José Filho <jc.bsfilho@gmail.com>
2025-03-24 18:02:01 -0300

    fix: add Babel presets for TypeScript support and remove ts-loader

--- packages/bundler/src/bundlers/webpack/webpack.config.ts
+++ packages/bundler/src/bundlers/webpack/webpack.config.ts
@@ -1,40 +1,4 @@
-import fs from 'fs';
-import { createRequire } from 'module';
-import { join } from 'path';
 import webpack, { Configuration } from 'webpack';
-
-const require = createRequire(import.meta.url);
-
-/**
- * Define the loader typescript rules if the tsconfig.json file exists
- * @returns {import('webpack').Configuration} - Module rules
- */
-const defineLoaderTypescriptRules = () => {
-  const tsConfigPath = join(process.cwd(), 'tsconfig.json');
-  const tsConfigExist = fs.existsSync(tsConfigPath);
-
-  if (tsConfigExist) {
-    return {
-      module: {
-        rules: [
-          {
-            test: /\.ts?$/,
-            use: [
-              {
-                loader: require.resolve('ts-loader'),
-                options: {
-                  transpileOnly: true,
-                },
-              },
-            ],
-            exclude: /node_modules/,
-          },
-        ],
-      },
-    };
-  }
-  return { module: {} };
-};
 
 const config: Configuration = {
   experiments: {
@@ -44,7 +8,6 @@ const config: Configuration = {
     extensions: ['.tsx', '.ts', '.js'],
     mainFields: ['browser', 'module', 'main'],
   },
-  ...defineLoaderTypescriptRules(),
   mode: 'production',
   target: ['webworker', 'es2022'],
   plugins: [
