88856bb3cee52fedd1f34df36bcd683a77535627
João Narciso <joaoznarciso@gmail.com>
2025-05-13 18:17:49 -0300

    refactor: rm env-vars copy files

--- packages/bundler/src/polyfills/azion/env-vars/context/env-vars.context.js
+++ packages/bundler/src/polyfills/azion/env-vars/context/env-vars.context.js
@@ -1,8 +1,3 @@
-import dotenv from 'dotenv';
-import fs, { existsSync } from 'fs';
-import { join } from 'path';
-import { fileURLToPath } from 'url';
-
 /**
  * This class is a VM context (ENV_VARS_CONTEXT) to handle with environment variables
  * @class EnvVarsContext
@@ -11,40 +6,9 @@ import { fileURLToPath } from 'url';
 class EnvVarsContext {
   #envVars;
 
-  #envFile = '.env';
-
-  #pathDefaultEdge = '.edge';
-
   constructor() {
-    const projectRoot = process.cwd();
-    const isWindows = process.platform === 'win32';
-    const outputPath = isWindows ? fileURLToPath(new URL(`file:///${join(projectRoot, '.')}`)) : join(projectRoot, '.');
-    const envFilePathRoot = join(outputPath, this.#envFile);
-    const envFilePathEdge = join(outputPath, `${this.#pathDefaultEdge}/${this.#envFile}`);
-
-    // Load .env file
-    dotenv.config({ path: [envFilePathRoot, envFilePathEdge] });
-
-    this.#cloneEnvRootToEdgeEnv(outputPath);
-
     this.#envVars = process.env;
   }
-
-  /**
-   * Clone .env file to .edge/.env
-   * @param {string} outputPath Root path of user project
-   * @returns {void} - No return
-   */
-  #cloneEnvRootToEdgeEnv = async (outputPath) => {
-    const envFilePathRoot = join(outputPath, this.#envFile);
-    if (!existsSync(envFilePathRoot)) return;
-    const envFileBuffer = await fs.promises.readFile(envFilePathRoot);
-    const envFilePath = join(outputPath, `${this.#pathDefaultEdge}/${this.#envFile}`);
-    await fs.promises.mkdir(join(outputPath, this.#pathDefaultEdge), {
-      recursive: true,
-    });
-    await fs.promises.writeFile(envFilePath, envFileBuffer);
-  };
 
   /**
    * Azion env vars get method
