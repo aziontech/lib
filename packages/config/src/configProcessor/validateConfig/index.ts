76886894df032da43ab7ef65938cabfe1221a134
João Narciso <joaoznarciso@gmail.com>
2025-06-27 14:26:56 -0300

    fix(config): rm validation from defineConfig

--- packages/config/src/configProcessor/validateConfig/index.ts
+++ packages/config/src/configProcessor/validateConfig/index.ts
@@ -3,6 +3,7 @@ import ajvErrors from 'ajv-errors';
 import addKeywords from 'ajv-keywords';
 
 import { AzionConfig } from '../../types';
+import convertLegacyConfig from '../helpers/convertLegacyConfig';
 import azionConfigSchema from '../helpers/schema';
 
 /**
@@ -17,17 +18,19 @@ function validateConfig(
   config: AzionConfig | Record<string, unknown>,
   schema: Record<string, unknown> = azionConfigSchema,
 ) {
+  /*  Converts legacy configuration properties to the new `behavior` format. */
+  const configConverted = convertLegacyConfig(config);
   const ajv = new Ajv({ allErrors: true, $data: true, allowUnionTypes: true });
   ajvErrors(ajv);
   addKeywords(ajv, ['instanceof']);
   const validate = ajv.compile(schema);
-  const valid = validate(config);
+  const valid = validate(configConverted);
 
   if (!valid) {
     if (validate.errors && validate.errors.length > 0) {
-      throw new Error(validate.errors[0].message);
+      throw new Error('Azion Config validation: ' + validate.errors[0].message);
     } else {
-      throw new Error('Configuration validation failed.');
+      throw new Error('Azion Config validation failed.');
     }
   }
 }
