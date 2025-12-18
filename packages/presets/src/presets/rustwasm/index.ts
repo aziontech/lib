daaab60c019d4151b21b56ffba19208663e08335
José Filho <jc.bsfilho@gmail.com>
2025-12-18 10:20:36 -0300

    chore: migrate rustwasm preset from esbuild bundling to storage-based WASM loading

--- packages/presets/src/presets/rustwasm/index.ts
+++ packages/presets/src/presets/rustwasm/index.ts
@@ -2,7 +2,7 @@ import type { AzionBuildPreset } from 'azion/config';
 import config from './config';
 import handler from './handler';
 import metadata from './metadata';
-// import prebuild from './prebuild';
 // import postbuild from './postbuild';
+import prebuild from './prebuild';
 
-export const rustwasm: AzionBuildPreset = { config, metadata, handler };
+export const rustwasm: AzionBuildPreset = { config, metadata, handler, prebuild };
